ef6fe17f5e1cfa54668f37f0aa0826308e4eaad6
dorian-rose <dorianrkelly@gmail.com>
2023-02-21 11:16:41 +0100

    updates - paint on refresh

--- script.js
+++ script.js
@@ -1,61 +1,70 @@
-let shopping = [];
-//const storedShopping = JSON.parse(localStorage.getItem("shopping")) || [];
+document.addEventListener("DOMContentLoaded", () => {
+  let shopping = JSON.parse(localStorage.getItem("shopping")) || [];
 
-//document variables
-let products = document.querySelector("#products");
-let storedItems = document.querySelector("#stored-items");
-let empty = document.querySelector("#empty");
+  //document variables
+  let products = document.querySelector("#products");
+  let storedItems = document.querySelector("#stored-items");
+  let empty = document.querySelector("#empty");
+  let fragment = document.createDocumentFragment();
 
-//event listeners
-products.addEventListener("click", ({ target }) => {
-  chooseFunction({ target });
-});
-empty.addEventListener("click", () => {
-  deleteAll();
-});
-
-//functions
-const chooseFunction = ({ target }) => {
-  if (target.matches(".add")) {
-    addToList({ target });
-  } else {
-    if (target.matches(".remove")) {
-      removeFromList({ target });
-    }
-  }
-};
-const addToList = ({ target }) => {
-  let product = {};
-  product.id = target.parentElement.id;
-  product.name = target.parentElement.children[0].textContent;
-  shopping.push(product);
-  localStorage.setItem("shopping", JSON.stringify(shopping));
-  const storedShopping = JSON.parse(localStorage.getItem("shopping")) || [];
-  printStoredList(storedShopping);
-};
-
-//funcion "remove" quita todos las frutas de un ID seleccionado. Con mas tiempo, buscaría hacer que si hay mas de una fruta del mismo tipo, solo quite una
-const removeFromList = ({ target }) => {
-  //no funcionó con "storedShopping" declarado desde fuera
-  const storedShopping = JSON.parse(localStorage.getItem("shopping")) || [];
-  let id = target.parentElement.id;
-  const newList = storedShopping.filter((item) => item.id != id);
-  localStorage.setItem("shopping", JSON.stringify(newList));
-  shopping = newList;
-  printStoredList(newList);
-};
-
-const deleteAll = () => {
-  localStorage.clear();
-  shopping = [];
-  printStoredList([]);
-};
-
-const printStoredList = (productsToPrint) => {
-  storedItems.innerHTML = "";
-  productsToPrint.forEach((item) => {
-    let listItem = document.createElement("li");
-    listItem.textContent = item.name;
-    storedItems.append(listItem);
+  //event listeners
+  products.addEventListener("click", ({ target }) => {
+    chooseFunction({ target });
   });
-};
+  empty.addEventListener("click", () => {
+    deleteAll();
+  });
+
+  //functions
+  const chooseFunction = ({ target }) => {
+    if (target.matches(".add")) {
+      addToList({ target });
+    } else {
+      if (target.matches(".remove")) {
+        removeFromList({ target });
+      }
+    }
+  };
+  const addToList = ({ target }) => {
+    let product = {};
+    product.id = target.parentElement.id;
+    product.name = target.parentElement.children[0].textContent;
+    shopping.push(product);
+    setLocal(shopping);
+    printStoredList();
+  };
+
+  const removeFromList = ({ target }) => {
+    let id = target.parentElement.id;
+    const newList = shopping.filter((item) => item.id != id);
+    setLocal(newList);
+    shopping = getLocal();
+    printStoredList();
+  };
+
+  setLocal = (updatedList) => {
+    return localStorage.setItem("shopping", JSON.stringify(updatedList));
+  };
+  getLocal = () => {
+    return JSON.parse(localStorage.getItem("shopping")) || [];
+  };
+
+  const deleteAll = () => {
+    localStorage.clear();
+    shopping = [];
+    printStoredList([]);
+  };
+
+  const printStoredList = () => {
+    storedItems.innerHTML = "";
+    const productsToPrint = getLocal();
+    productsToPrint.forEach((item) => {
+      let listItem = document.createElement("li");
+      listItem.textContent = item.name;
+      fragment.append(listItem);
+    });
+    storedItems.append(fragment);
+  };
+
+  printStoredList();
+}); //LOAD
